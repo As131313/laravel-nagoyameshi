0ebaf6d8da574b34bb4468f38441c4714da2d3ad
As131313 <hiro0131.naka@gmail.com>
2024-10-29 19:31:40 +0900

    エラー確認用

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
     });
-    
-use Illuminate\Support\Facades\Route;
- // ルーティングを設定するコントローラを宣言する
- use App\Http\Controllers\HelloController;
- use App\Http\Controllers\ProductController;
- use App\Http\Controllers\VendorController;
- use App\Http\Controllers\RequestController;
- use App\Http\Controllers\ResponseController;
- use App\Http\Controllers\SignInController;
- use App\Http\Controllers\CookieController;
- use App\Http\Controllers\SessionController;
 
 
 /*
